49a20600ad333edfaf2cf2f2b41f5b9358bb8ab3
firchtn <firaschaabencss@gmail.com>
2017-08-04 17:33:05 +0100

    fix img display

--- singup/forms.py
+++ singup/forms.py
@@ -7,5 +7,5 @@ class usersForms(forms.Form):
 	username = forms.CharField(max_length=10, required=False, widget=forms.TextInput(attrs={'class' : 'form-control'}))
 	email = forms.EmailField(label=u"mail", required=False, widget=forms.TextInput(attrs={'class' : 'form-control'}))
 	password = forms.CharField(required=False, widget=forms.PasswordInput(attrs={'class' : 'form-control' , 'placeholder': 'Password' }))
-	selfi = forms.ImageField(required=False, widget=forms.FileInput(attrs={'class':'btn btn-default btn-file'}))
+	selfi = forms.ImageField(required=False, widget=forms.FileInput(attrs={ 'id' : 'imgg', 'class':'btn btn-default btn-file'}))
 
