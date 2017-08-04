c549a3fd53876501cd855eeb3fdb14293351c994
firchtn <firaschaabencss@gmail.com>
2017-08-04 17:21:44 +0100

    fix form

--- space/forms.py
+++ space/forms.py
@@ -2,8 +2,8 @@ from django import forms
 
 
 class post_forms(forms.Form):
-	title = forms.CharField(max_length=20)
-	aboutnow = forms.CharField(max_length=200)
+	title = forms.CharField(max_length=20, widget=forms.TextInput(attrs={'class' : 'form-control'}))
+	aboutnow = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'class' : 'form-control'}))
 
 class message_forms(forms.Form):
-	msg = forms.CharField(max_length=2000)
+	msg = forms.CharField(max_length=2000, widget=forms.TextInput(attrs={'class' : 'form-control'}))
