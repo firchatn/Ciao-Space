0b745656e15c79ccccf61c986fe812b4783eaaff
Moez Bouhlel <bmoez.j@gmail.com>
2017-08-17 00:31:43 +0100

    add debug_toolbar urls

--- ciao_space/urls.py
+++ ciao_space/urls.py
@@ -13,3 +13,9 @@ urlpatterns = [
 
 urlpatterns += static(settings.MEDIA_URL,
                       document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG and settings.DEBUG_TOOLBAR_INSTALLED:
+    import debug_toolbar
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ]
